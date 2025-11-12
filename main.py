e45034b7f34e4974e4926ba318a6ef43c9db91e0
IMarty <igor.marty@telecomnancy.net>
2025-11-12 16:39:20 +0100

    feat: Added good afternoon (#4)

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ import datetime
 # Get the current hour (24-hour format)
 current_hour = datetime.datetime.now().hour
 # Other check, code, documentation...
-
-if current_hour < 18:  # Before 6 PM
+if current_hour < 13:  # Before 6 PM
     print("Hello")
-else:  # 6 PM or later
+elif current_hour >18:  # 6 PM or later
     print("Good night")
+elif current_hour >=13 :
+    print("Good afternoon")    
+else: 
+    print("Time?")    
