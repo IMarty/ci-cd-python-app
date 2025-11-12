b98fb5bd333816b6c14308afa69914c67c0abd7e
IMarty <igor.marty@telecomnancy.net>
2025-11-12 16:55:55 +0100

    YOLO

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import datetime
 
-# Get the current hour (24-hour format)
-current_hour = datetime.datetime.now().hour
-# Other check, code, documentation...
-if current_hour < 13:  # Before 6 PM
-    print("Hello")
-elif current_hour >18:  # 6 PM or later
-    print("Good night")
-elif current_hour >=13 :
-    print("Good afternoon")    
-else: 
-    print("Time?")    
+def get_greeting():
+    # Get the current hour (24-hour format)
+    current_hour = datetime.datetime.now().hour
+    # Other check, code, documentation...
+    if current_hour < 13:  # Before 6 PM
+        return "Hello"
+    elif current_hour >18:  # 6 PM or later
+        return "Good night"
+    elif current_hour >=13 :
+        return "Good afternoon"    
+    else: 
+        return "Time?"    
+if __name__ == "__main__":
+    print(get_greeting())
