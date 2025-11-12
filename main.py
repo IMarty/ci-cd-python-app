c62800f3f70d032bcaca89a18f83a6318bec59aa
IMarty <igor.marty@telecomnancy.net>
2025-11-12 16:17:51 +0100

    Feat 1 : almost complet (#2)
    * feat: should work but not tested.
    
    * doc: update on comment

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("hello")
+import datetime
+
+# Get the current hour (24-hour format)
+current_hour = datetime.datetime.now().hour
+# Other check, code, documentation...
+
+if current_hour < 18:  # Before 6 PM
+    print("Hello")
+else:  # 6 PM or later
+    print("Good night")
